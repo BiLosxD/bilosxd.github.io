b5ac4c952425a90f816f0ec4c81cd6b9e26e0c60
BiLos <bstamariadesignblue@gmail.com>
2020-06-23 21:25:51 +0800

    builded 1.3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,12 @@
 <template>
     <div id="home">
+        <h1 class="no_font">BRSTAMARIA</h1>
         <section id="banner">
-            
+            <div class="overlay">
+                <h2>Welcome</h2>
+                <h3>to my site</h3>
+                <div class="default_btn">Check out my Portfolio</div>
+            </div>
         </section>
     </div>
 </template>
